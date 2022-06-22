858935733506aff6b49c3137d63cffe94ece7a25
Christopher Goulart <cgoulart35@gmail.com>
2022-06-21 23:47:49 -0400

    Updating GBot script after docker compose upgrade

--- scripts/UpdateGBot.sh
+++ scripts/UpdateGBot.sh
@@ -7,5 +7,5 @@ chmod -R 777 ~/Code/GBot
 
 cd ~/Code/GBot
 
-docker-compose -f docker-compose-prod.yml down
-docker-compose -f docker-compose-prod.yml up -d --build
+docker compose -f docker-compose-prod.yml down
+docker compose -f docker-compose-prod.yml up -d --build
