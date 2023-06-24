2d5ff68432b3df69c2a6ad6b33417958493f12ce
Christopher Goulart <cgoulart35@gmail.com>
2023-06-24 02:46:18 -0400

    Adding support for 2 GBot instances (1 dev 1 prod)

--- scripts/UpdateGBot.sh
+++ scripts/UpdateGBot.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-cp ~/Code/GitProjectUpdateHandler/Shared/GBot/gbot.env ~/Code/GBot/Shared/
+cd ~/Code/GBot
 cp ~/Code/GitProjectUpdateHandler/Shared/GBot/serviceAccountKey.json ~/Code/GBot/Shared/
 
+# update prod instance
+
+cp ~/Code/GitProjectUpdateHandler/Shared/GBot/gbot.env.prod ~/Code/GBot/Shared/gbot.env
 chmod -R 777 ~/Code/GBot
 
-cd ~/Code/GBot
+docker compose -f docker-compose-prod.yml -p gbotprod01 down
+docker compose -f docker-compose-prod.yml -p gbotprod01 up -d --build
 
-docker compose -f docker-compose-prod.yml down
-docker compose -f docker-compose-prod.yml up -d --build
+# update dev instance
+
+cp ~/Code/GitProjectUpdateHandler/Shared/GBot/gbot.env.dev ~/Code/GBot/Shared/gbot.env
+chmod -R 777 ~/Code/GBot
+
+docker compose -f docker-compose-dev.yml -p gbotdev01 down
+docker compose -f docker-compose-dev.yml -p gbotdev01 up -d --build
